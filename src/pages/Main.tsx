8936291e710cac652005612be6099639f5078cda
pouyio <pouyio@gmail.com>
2022-07-02 20:05:24 +0200

    updated url

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -104,7 +104,7 @@ export const Main: FC = () => {
         }}
       >
         <source
-          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+          src="https://bailapp.s3.eu-central-1.amazonaws.com/BigBuckBunny.mp4"
           type="video/mp4"
         />
         Your browser does not support the video tag.
